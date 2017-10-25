f17a7822ac1aadfac74c27a6cc34ac0168a603a2
Giovanni Carmo <giovannicarno96@gmail.com>
2017-10-24 23:07:54 -0200

    Testando o funcionamento do Srping Boot

---
+++ src/main/java/com/giovannicarmo/projetocurso/carmoeletro/CarmoeletroApplication.java
@@ -0,0 +1,12 @@
+package com.giovannicarmo.projetocurso.carmoeletro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmoeletroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmoeletroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/giovannicarmo/projetocurso/carmoeletro/CarmoeletroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.giovannicarmo.projetocurso.carmoeletro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarmoeletroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
