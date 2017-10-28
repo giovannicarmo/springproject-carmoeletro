79a1b3745e31909e20cb1779d4331c39a34792ca
Giovanni Carmo <giovannicarno96@gmail.com>
2017-10-28 15:37:58 -0200

    Ajuste no endpoint /categorias/{id} e implementaçao das classes de excessao

--- src/main/java/com/giovannicarmo/projetocurso/carmoeletro/services/CategoryService.java
+++ src/main/java/com/giovannicarmo/projetocurso/carmoeletro/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.giovannicarmo.projetocurso.carmoeletro.services;
 
 import com.giovannicarmo.projetocurso.carmoeletro.domain.Category;
 import com.giovannicarmo.projetocurso.carmoeletro.repositories.CategoryRepository;
+import com.giovannicarmo.projetocurso.carmoeletro.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,9 @@ public class CategoryService {
 
     public Category find (Integer id){
         Category category = categoryRepository.findOne(id);
+        if (category == null) {
+            throw new ObjectNotFoundException("Object not found! Id: " + id + " Type: " + Category.class.getName());
+        }
         return category;
     }
 }
