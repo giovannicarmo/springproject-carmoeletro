f778bc4d7921d07280a856003d0f268f2820ea35
Giovanni Carmo <giovannicarno96@gmail.com>
2018-01-25 18:41:22 -0200

    sdfasf

---
+++ src/main/java/com/giovannicarmo/projetocurso/carmoeletro/resources/OrderResource.java
@@ -0,0 +1,24 @@
+package com.giovannicarmo.projetocurso.carmoeletro.resources;
+
+import com.giovannicarmo.projetocurso.carmoeletro.domain.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+
+        Order order = orderService.find(id);
+        return ResponseEntity.ok().body(order);
+    }
+}
