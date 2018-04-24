84453f0c7cc915a98063ad1186db04e5dcc6730c
giovannicarmo <giovannicarmo96@gmail.com>
2018-04-23 22:28:08 -0300

    Primeiro upload de imagem

--- src/main/java/com/giovannicarmo/projetocurso/carmoeletro/CarmoeletroApplication.java
+++ src/main/java/com/giovannicarmo/projetocurso/carmoeletro/CarmoeletroApplication.java
@@ -1,5 +1,7 @@
 package com.giovannicarmo.projetocurso.carmoeletro;
 
+import com.giovannicarmo.projetocurso.carmoeletro.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarmoeletroApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(CarmoeletroApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
+        s3Service.uploadFile("C:\\Users\\giova\\OneDrive\\Imagens\\Imagens salvas\\God-Of-War2.jpg");
     }
 }
